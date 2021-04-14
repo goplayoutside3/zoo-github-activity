e3b8b652342f955601d159cdd69c2d0fcaa1d2eb
Delilah Clement <delilahclement@gmail.com>
2021-04-14 14:55:12 -0500

    initial package setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import zooTheme from '@zooniverse/grommet-theme'
+import { base as baseTheme } from 'grommet'
+import merge from 'lodash/merge'
+import { Grommet } from 'grommet'
+
+const theme = merge({}, baseTheme, zooTheme)
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Grommet theme={theme}>
+      <Component {...pageProps} />
+    </Grommet>
+  )
 }
 
 export default MyApp
