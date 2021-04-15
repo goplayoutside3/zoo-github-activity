4d980042190ddc79a37f3a32231509313d09e4d3
Delilah Clement <delilahclement@gmail.com>
2021-04-15 08:30:21 -0500

    sort pull requests by opened or closed

--- components/PullRequests.js
+++ components/PullRequests.js
@@ -1,7 +1,30 @@
 import { Box } from 'grommet'
+import { useEffect, useState } from 'react'
 
 const PullRequests = ({ data }) => {
-  return <Box>{data.length} pull requests. </Box>
+  const [openedRequests, setOpenedRequests] = useState(0)
+  const [closedRequests, setClosedRequests] = useState(0)
+
+  useEffect(() => {
+    if (!data) return
+    let opened = 0
+    let closed = 0
+    
+    data.forEach(item => {
+      if (item.payload.action === "opened") opened++
+      if (item.payload.action === "closed") closed++
+    });
+
+    setOpenedRequests(opened)
+    setClosedRequests(closed)
+  }, [data])
+
+  return (
+    <Box>
+      <Box>{openedRequests} opened pull requests. </Box>
+      <Box>{closedRequests} closed pull requests. </Box>
+    </Box>
+  )
 }
 
 export default PullRequests
